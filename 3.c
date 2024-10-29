b89c38550e4853fadeda727daee651f7816f1ab7
csz2006 <1805696837@qq.com>
2024-10-29 08:52:48 +0800

    Add files via upload

--- 3.c
+++ 3.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    double x,y;
-    scanf("%lf",&x);
-    if(x<=100000){y=0.1*x;}
-    else if(x>100000&&x<=200000){x-=100000;y=10000+0.075*x;}
-    else if(x>200000&&x<=400000){x-=200000;y=10000+7500+0.05*x;}
-    else if(x>400000&&x<=600000){x-=400000;y=10000+7500+10000+0.03*x;}
-    else if(x>600000&&x<=1000000){x-=600000;y=10000+7500+10000+6000+0.015*x;}
-    else if(x>1000000){x-=1000000;y=10000+7500+10000+6000+6000+0.01*x;}
-    printf("%0.2f",y);
+    int N,a;
+    scanf("%d",&N);
+    a=N%5;
+    if(a<=3&&a!=0){printf("Fishing in day %d",N);}
+    else{printf("Drying in day %d",N);}
     return 0;
 }
+    
