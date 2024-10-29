085531a219c6c44ab2cdfb266518ab91da5f28d6
csz2006 <1805696837@qq.com>
2024-10-29 15:37:30 +0800

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    int i,n;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=a&&i<=b;i++){
+        if(a%i==0){
+            if(b%i==0){n=i;
+            }else;}
+        else; 
+    }
+    printf("%d",n);
+    return 0;
+}
